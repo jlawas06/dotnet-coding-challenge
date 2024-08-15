d7f1962dc47a2fafef25cf3635e0ff1a1df8dd96
jlawas06 <jlawas06@gmail.com>
2024-08-15 22:41:24 +0800

    feat: add application and infrastructure

--- src/PizzaXYZ.Backend/PizzaXYZ.Backend.Api/Program.cs
+++ src/PizzaXYZ.Backend/PizzaXYZ.Backend.Api/Program.cs
@@ -1,6 +1,11 @@
+using PizzaXYZ.Backend.Application;
+using PizzaXYZ.Backend.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
