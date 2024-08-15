34352ddf45ef2513bf8d1eddc99fe7dc8639499b
jlawas06 <jlawas06@gmail.com>
2024-08-15 22:23:53 +0800

    feat: add appDbContext and configure sql server

---
+++ src/PizzaXYZ.Backend/PizzaXYZ.Backend.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace PizzaXYZ.Backend.Infrastructure.Data;
+internal class AppDbContext : DbContext, IAppDbContext
+{
+    public DbSet<Pizza> Pizzas { get; set; }
+    public DbSet<PizzaType> PizzaTypes { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
+}
