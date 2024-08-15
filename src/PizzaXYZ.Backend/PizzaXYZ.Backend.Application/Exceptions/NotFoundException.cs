b3b6c7800b2bc73e35428dccc4981c3eab2c12f1
jlawas06 <jlawas06@gmail.com>
2024-08-16 00:21:41 +0800

    feat: add custom exceptions

---
+++ src/PizzaXYZ.Backend/PizzaXYZ.Backend.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace PizzaXYZ.Backend.Application.Exceptions;
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string message) : base(message) { }
+
+    public NotFoundException(string name, object key)
+        : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+    }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
