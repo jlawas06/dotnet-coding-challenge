21b94b5b3c7975537d0d4c4eea766fa5d51d3bda
jlawas06 <jlawas06@gmail.com>
2024-08-15 23:38:19 +0800

    feat: add ref properties

--- src/PizzaXYZ.Backend/PizzaXYZ.Backend.Domain/Entities/OrderDetails.cs
+++ src/PizzaXYZ.Backend/PizzaXYZ.Backend.Domain/Entities/OrderDetails.cs
@@ -4,4 +4,6 @@ public class OrderDetails : BaseEntity<int>
     public int OrderId { get; set; }
     public required string PizzaId { get; set; }
     public int Quantity { get; set; }
+    public virtual Pizza Pizza { get; set; } = default!;
+    public virtual Order Order { get; set; } = default!;
 }
