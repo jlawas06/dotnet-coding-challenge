042b00892eca65a2c767e759bb0958374706089d
jlawas06 <jlawas06@gmail.com>
2024-08-15 22:22:13 +0800

    chore: update global using references

---
+++ src/PizzaXYZ.Backend/PizzaXYZ.Backend.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using PizzaXYZ.Backend.Domain.Entities;
+global using PizzaXYZ.Backend.Application.Interfaces;
+global using PizzaXYZ.Backend.Infrastructure.Data;
+global using Microsoft.EntityFrameworkCore;
