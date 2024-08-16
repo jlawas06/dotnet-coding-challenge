9dd443245666f12c3c66133f29031f5f704e9fbe
jlawas06 <jlawas06@gmail.com>
2024-08-16 13:49:38 +0800

    feat: pizza rest api endpoint implementation

---
+++ src/PizzaXYZ.Backend/PizzaXYZ.Backend.Application/CQRS/Pizzas/GetPizzaById/GetPizzaByIdQuery.cs
@@ -0,0 +1,30 @@
+﻿
+namespace PizzaXYZ.Backend.Application.CQRS.Pizzas.GetPizzaById;
+public class GetPizzaByIdQuery : IRequest<PizzaDto>
+{
+    public required string Id { get; set; }
+}
+
+internal class GetPizzaByIdQueryHandler : IRequestHandler<GetPizzaByIdQuery, PizzaDto>
+{
+    private readonly IPizzaService _pizzaService;
+    private readonly IMapper _mapper;
+
+    public GetPizzaByIdQueryHandler(IPizzaService pizzaService)
+    {
+        _pizzaService = pizzaService;
+
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Pizza, PizzaDto>();
+        });
+
+        _mapper = config.CreateMapper();
+    }
+
+    public async Task<PizzaDto> Handle(GetPizzaByIdQuery request, CancellationToken cancellationToken)
+    {
+        var pizza = await _pizzaService.GetPizzaByIdAsync(request.Id) ?? throw new NotFoundException($"Pizza {request.Id} not exists.");
+        return _mapper.Map<PizzaDto>(pizza);
+    }
+}
